130cd21bef111f3b91d195c3ecebdad08e2372c4
Olexander96 <o.sheiko96@gmail.com>
2023-03-22 20:35:12 +0200

    created componentDidMount for axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,16 +4,19 @@ import styles from './User.module.css';
 import userPhoto from '../../assets/images/user.jpg'
 
 class Users extends  React.Component  {
-    constructor(props) {
-        super(props)
+    // constructor(props) { //це відбувається автоматично, можна не писати це але потрібно розуміти, пишемо тільки тоді коли хочемо шось змінити
+    //     super(props)
+    //     this.props = props
+    // }
 
+    componentDidMount() {
         if (this.props.users.length === 0) {
             axios
             .get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {this.props.setUsers(response.data.items)})
         }
     }
-        
+  
     render() {
         return this.props.users.map(user => (
                 <div key={user.id}>
